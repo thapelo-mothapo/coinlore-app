118e653693024c83c42cacd21cb656e7a05f6063
Thapelo Mothapo <thapelo.mothapo@sovtech.com>
2022-02-22 21:26:10 +0200

    fix index p tag grammar

--- pages/index.js
+++ pages/index.js
@@ -12,10 +12,7 @@ export default function Home() {
       </Head>
       <div>
         <h1 className={styles.title}>Welcome to WeCoin</h1>
-        <p className={styles.text}>
-          Designed and developed around{' '}
-          <Link href="https://www.coinlore.com/cryptocurrency-data-api"><a>Coinlore</a></Link>{' '}
-          which provides a free cryptocurrency api <br />that does not need api keys and is available publicly.
+        <p className={styles.text}>Designed and developed around<Link href="https://www.coinlore.com/cryptocurrency-data-api"><a> Coinlore </a></Link>which provides a free cryptocurrency api <br />that does not need api keys and is available publicly.
         </p>
         
         <Link href="/coins/">
